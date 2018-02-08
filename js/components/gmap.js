81c6c4df64d99a50f551ae2994676d59c4c9664a
maltisaidava <maltisaidava@gmail.com>
2018-02-08 11:37:02 -0800

    map added

--- js/components/gmap.js
+++ js/components/gmap.js
@@ -30,7 +30,7 @@ window.initMap = function() {
 
   var customMapTypeId = 'custom_style';
 
-  var brooklyn = {lat: 41.850, lng: -73.961};
+  var brooklyn = {lat: 23.1066, lng: 72.533};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
     scrollwheel: false,
